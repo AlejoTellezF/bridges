c9b23a3e935ab21f2cfd3d8116198959b0cf2f50
alejotellezf <omartellezf@gmail.com>
2020-10-25 17:37:55 -0500

    Added a mocked bridge-api in order to response http methods

---
+++ bridge-api/routes/bridges.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('../db');
+
+/* http://localhost:3000/api/bridges */
+router.get('/bridges', function(req, res) {
+  res.json(db.all());
+});
+
+/* http://localhost:3000/api/bridges/id... */
+router.get('/bridges/:id', function(req, res) {
+
+  const id = req.params.id;
+  const bridge = db.byId(id);
+
+  if(!bridge){
+    return res.sendStatus(404);
+  }
+
+  res.json(bridge);
+});
+
+module.exports = router;
